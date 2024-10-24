7fc0bf4063ca32e926a3dc29c1be45eaa55456d6
Kirill Tatarenko <hrothnar@mail.ru>
2024-10-24 06:59:03 +0400

    process: 2

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
 
+int multiply() {
+	int x {};
+	std::cout << "Provide a number\n";
+	std::cin >> x;
+	std::cout << "Your number multiplied by 2 is: " << x * 2<< "\n";
+	std::cout << "Your number multiplied by 3 is: " << x * 3<< std::endl;
+	return 0;
+}
+
 int main() {
-	std::cout << "Hello, world!";
+	// int x {};
+	// int y;
+
+	// std::cout << x;
+	// std::cout << y;
+	// std::cout << "Please, provide an input!" << std::endl;
+
+	// std::cin >> x >> y;
+
+	// std::cout << "Hello, world!" << std::endl << "And you too!" << std::endl;
+	// std::cout << "Your input: " << x << " | " << y << "\n";
+	multiply();
+
 	return 0;
 }
 
@@ -16,3 +37,4 @@ int main() {
 
 //     return 0;
 // }
+
