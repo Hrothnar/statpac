f0f42fdb5ecbcd35d08bb27e63f24473921a0bb5
Kirill Tatarenko <kirill.tatarenko@rockbitegames.com>
2024-11-04 13:45:57 +0400

    process: 6

--- src/print.h
+++ src/print.h
@@ -1,2 +1,12 @@
+#ifndef PRINT_H
+#define PRINT_H
 
 void printPassedInt(int);
+void printPassedInt(int); // forward could be duplicated as they only declare the existence of a function
+
+#endif
+
+// #pragma once - an alternative way to guard headers from duplication. 
+// There is one known case where #pragma once will typically fail. If a header file is copied so that it exists in multiple places on the file system,
+// if somehow both copies of the header get included, header guards will successfully de-dupe the identical headers, 
+// but #pragma once won’t (because the compiler won’t realize they are actually identical content).
